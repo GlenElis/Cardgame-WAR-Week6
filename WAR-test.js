708a5ee8a804e6aaec4e4b96cfddeff6f6f1ee16
Glennys Fuentes <glennys89@hotmail.com>
2022-03-18 01:19:33 -0400

    Test to print a card- successful

---
+++ WAR-test.js
@@ -0,0 +1,10 @@
+var expect = chai.expect;
+
+describe('MyFunction', function() {
+    describe('showCard', function() {
+        it('should print a card', function() {
+            const newCard = new Card();
+            expect(newCard.value).to.equal();
+        }); 
+    });
+});
